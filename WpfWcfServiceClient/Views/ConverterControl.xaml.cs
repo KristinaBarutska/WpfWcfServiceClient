61d70b86a96f42a8bd0bf2d79db417ff2e095c04
kristina.p.georgieva <kristina.p.georgieva@gmail.com>
2017-12-13 15:14:51 +0200

    Implemented MVVM pattern

---
+++ WpfWcfServiceClient/Views/ConverterControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfWcfServiceClient.Views
+{
+    /// <summary>
+    /// Interaction logic for ConverterControl.xaml
+    /// </summary>
+    public partial class ConverterControl : UserControl
+    {
+        public ConverterControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
